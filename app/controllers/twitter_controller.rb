01d120fc1fb9192498a38803e44fd0f2ac19db4a
Michael Macasek <macasek@where.com>
2011-11-18 15:09:40 -0500

    removed unused code

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,15 +1,4 @@
 class TwitterController < ApplicationController
-  respond_to :html, :only => [:index]
-  respond_to :json, :only => [:index]
-  
-  helper_method :tweets
-  
   def index
-    respond_with tweets
-  end
-
-private
-  def tweets
-    @tweets ||= HTTParty.get("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=macasek&count=5&include_rts=true") 
   end
 end
