bcb1fecf27914aa4dc7cc8e3bb479c752f4373b7
Michael Macasek <macasek@where.com>
2011-11-21 09:53:41 -0500

    removed rails app as it was not needed

--- app/controllers/twitter_controller.rb
+++
@@ -1,4 +0,0 @@
-class TwitterController < ApplicationController
-  def index
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HelloBackbonejs::Application.config.secret_token = 'a5155b6c1e7858ae1da1569e628ec44bd172b3c21007da131cda19943c1c6bc7dbc1cd00734717663b79fe586ead831ecc192ff7ec4a85d2058229b3241c4129'
