4788bdedc59fe27a45b4ffd7a16b532290e7db92
Michael Macasek <macasek@where.com>
2011-11-21 11:32:48 -0500

    added quickie hack for a github project widget. need to refactory and combine into a generic widget

---
+++ javascripts/github.js
@@ -0,0 +1,116 @@
+$(function() {
+  (function($){
+    var Project = Backbone.Model.extend();
+
+    var GitHub = Backbone.Collection.extend({
+      model: Project,
+      url: "http://github.com/api/v1/json/macasek?callback=?"
+    });
+  
+    var ProjectView = Backbone.View.extend({
+      tagName: 'li',
+      className: 'project',
+      templateStr: '<h3><a href="<%= project.get("url") %>"><%= project.get("name") %></a></h3><p><%= project.get("description") %></p><p>Updated: <span class="time" data-date="<%= project.get("pushed_at") %>"><%= pretty_date %></span></p>',
+
+      initialize: function(){
+        _.bindAll(this, 'render', "template");
+      },
+    
+      render: function() {                                  
+        var element = $(this.el); 
+     
+        element.html(this.template(this.model));
+        element.find(".time").prettyDate();
+         
+        return this;
+      },
+      
+      template: function(project) {
+        return _.template(this.templateStr, { 
+                            project: project, 
+                            pretty_date: this.parseDate(project.get("pushed_at")) });
+      },
+      
+      parseDate: function(date_str) { 
+        if(date_str == undefined) {
+          return "";
+        }
+        
+        var d = new Date(date_str);
+        var hours = d.getHours();
+        var am_pm = "am";
+        
+        if(hours > 12) {
+          am_pm = "pm";
+          hours = hours - 12;
+        }
+        
+        return d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear() + " " + hours + ":" + d.getMinutes() + am_pm;
+      }
+    });  
+  
+    var GitHubView = Backbone.View.extend({
+      el: $('div#projects'),
+    
+      initialize: function() {
+        _.bindAll(this, 'render', 'appendProject', 'appendProjects', 'refresh');  
+      
+        this.collection = new GitHub();
+        this.collection.bind('reset', this.appendProjects);
+      
+        this.render();
+        this.refresh();
+      },
+    
+      render: function(){   
+        this.el.append("<ul id='project_list'></ul><p><a href='http://github.com/macasek'>see all</a>");
+        
+        return this;       
+      },
+      
+      appendProjects: function(projects) {
+        _(this.collection.models).each(function(project) {
+          this.appendProject(project);
+        }, this);
+      },
+        
+      appendProject: function(project) {     
+        var projectView = new ProjectView({
+          model: project
+        });
+        $('ul#project_list', this.el).append(projectView.render().el);
+      },
+      
+      refresh: function() {
+        var that = this;
+        var projects = $.getJSON(this.collection.url, function(data, textStatus, jqXHR) { 
+          if(textStatus == "success") {
+            $("#projects").effect("highlight", {color:"#E8F5FB"}, 3000);
+            $('ul#project_list', this.el).text("");     
+
+            var repos = data.user.repositories
+            
+            // filter out private and empty repos
+            repos = _.filter(repos, function(r){ 
+              return (r.size != 0) && (!r.private); 
+            });
+            
+            // convert to date objects for easier sorting 
+            _.each(repos, function(r){
+              r.pushed_at = new Date(r.pushed_at);
+            });
+
+            repos.sort(function(a,b) {
+              return b.pushed_at - a.pushed_at;
+            });  
+            
+            // set the most recent 5 to the collection
+            that.collection.reset(repos.slice(0,5));
+          }
+        });     
+      }  
+    });
+  
+    window.githubView = new GitHubView();                             
+  })(jQuery);
+});     
